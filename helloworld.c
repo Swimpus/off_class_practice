09ca165f246c0debdce89a2c9406d24ea587712f
Swimpus <yejo32602@gmail.com>
2026-05-13 21:51:44 +0000

    Initial Commit

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+# include <stdio.h> // Helps us deal with input and output.
+                    // Think of it like importing a library in python.
+                    // Ergo import math, import random.
+
+// Our main function!!
+// Equivalent to def main():
+// {} encapsules the code inside main
+int main(){
+    printf("And you reach out to touch me...\n"); // All statements end with a semicolon
+                                                 // Similar to how a period is used in sentences.
+    printf("But I am in the Twillight\n"); // newline character is the same.
+
+}
